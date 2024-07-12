68dc623f896c23f75e27506a6651d89f05ebc1cd
Itor Carlos <itor51carlos@gmail.com>
2024-07-12 20:26:14 -0300

    cadastrar rotas de listagem e de inserção de User

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    //
+    public function store(Request $request)
+    {
+        $validacao = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'data_nascimento' => 'required',
+        ]);
+
+        if($validacao->fails()){
+            return response()->json($validacao->erros(),400);
+        }
+
+        $usuario = User::create([
+            'cpf' => $request->cpf,
+            'name' => $request->name,
+            'data_nascimento' => $request->data_nascimento,
+        ]);
+        return response()->json($usuario,201);
+    }
+
+
+    public function index()
+    {
+        return response()->json(User::all());
+    }
+
+    public function salvar(){
+        return view('insert');
+    }
+}
